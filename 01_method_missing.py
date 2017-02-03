70661b24f29c836af71c8cc12ece6acc5887a6b8
Eric Duminil <eric.duminil@gmail.com>
2017-02-03 23:18:42 +0100

    basic insel model

--- 01_method_missing.py
+++ 01_method_missing.py
@@ -1,3 +1,8 @@
+#TODO: import insel. nothing more
 from insel import *
+from model import *
 
-Block.sum(3,5)
+#Block.sum(3,5)
+#Block.pi()
+
+Model().run()
---
+++ model.py
@@ -0,0 +1,45 @@
+import os
+import subprocess
+import tempfile
+import re
+
+class Insel:
+  dirname = "/usr/local/INSEL/resources/"
+  command = "./insel"
+  extension = ".insel"
+  regexp = re.compile('Running insel [\d\w \.]+ \.\.\.\s+([^\*]*)Normal end of run', re.I | re.DOTALL)
+
+class Model:
+  def run(self):
+    raw = self.raw_results()
+    match = Insel.regexp.search(raw)
+    if match:
+      output = match.group(1)
+      floats = self.extract([self.parse_line(line) for line in output.split("\n") if line])
+      print '%r' % floats
+    else:
+      raise Exception("Problem with INSEL : %s" % raw)
+
+  def parse_line(self, line):
+      return self.extract([float(x) for x in line.split() if x])
+
+  def extract(self, array):
+    if len(array) == 1:
+      return array[0]
+    else:
+      return array
+
+  def raw_results(self):
+    #TODO: come back to cwd
+    os.chdir(Insel.dirname)
+    with self.tempfile() as file:
+      file.write(self.content())
+      var = subprocess.check_output([Insel.command, file.name], shell = False)
+    return var
+
+  def tempfile(self):
+    return tempfile.NamedTemporaryFile(mode = 'w+', suffix = Insel.extension, prefix = 'python_', bufsize = 0)
+
+  def content(self):
+    return """s 1 PI 
+    s 2 SCREEN 1.1"""
