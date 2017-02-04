3d3fb621237cb4858e9c5e072849722605542022
Eric Duminil <eric.duminil@gmail.com>
2017-02-04 13:41:35 +0100

    sin

--- 01_method_missing.py
+++ 01_method_missing.py
@@ -5,3 +5,6 @@ from one_block_model import *
 
 print OneBlockModel('pi').run()
 print OneBlockModel('sum', inputs = [3,2]).run()
+print OneBlockModel('sum', inputs = [3,2]).run()
+
+print OneBlockModel('sin', inputs = [OneBlockModel('mul', inputs = [9, OneBlockModel('sum', inputs = [6,4]).run()]).run()]).run()
