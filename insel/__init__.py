d095ede8018e927c0cce482c4e93b3f1306088c2
Eric Duminil <eric.duminil@gmail.com>
2017-02-08 15:33:52 +0100

    insel.block and template

--- insel/__init__.py
+++ insel/__init__.py
@@ -1,2 +1,8 @@
 from one_block_model import OneBlockModel
 from template import Template
+
+def block(name, *args):
+    return OneBlockModel(name, inputs = args).run()
+
+def template(name, **parameters):
+    return Template(name, **parameters).run()
--- insel_example.py
+++ insel_example.py
@@ -1,6 +1,5 @@
 import insel
-print insel.OneBlockModel('pi').run()
-print insel.OneBlockModel('sum', inputs = [3,2]).run()
-#print OneBlockModel('sin', inputs = [OneBlockModel('mul', inputs = [9, OneBlockModel('sum', inputs = [6,4]).run()]).run()]).run()
 
-print insel.Template('a_times_b', a = 5, b = 3).run()
+print insel.block('pi')
+print insel.block('sum', 2, 3)
+print insel.template('a_times_b', a = 7, b = 3)
