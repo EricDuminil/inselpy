14c03d5568528e32e8cce1510f7dbd71fe459c95
Eric Duminil <eric.duminil@gmail.com>
2017-02-08 23:08:10 +0100

    autopep8

--- insel/model.py
+++ insel/model.py
@@ -3,45 +3,54 @@ import subprocess
 import tempfile
 import re
 
+
 class Insel:
-  dirname = "/usr/local/INSEL/resources/"
-  command = "./insel"
-  extension = ".insel"
-  regexp = re.compile('Running insel [\d\w \.]+ \.\.\.\s+([^\*]*)Normal end of run', re.I | re.DOTALL)
+    dirname = "/usr/local/INSEL/resources/"
+    command = "./insel"
+    extension = ".insel"
+    regexp = re.compile(
+        'Running insel [\d\w \.]+ \.\.\.\s+([^\*]*)Normal end of run',
+        re.I | re.DOTALL)
 
-#TODO: use private methods
+# TODO: use private methods
+
+
 class Model:
-  def run(self):
-    raw = self.raw_results()
-    match = Insel.regexp.search(raw)
-    if match:
-      output = match.group(1)
-      floats = self.extract([self.parse_line(line) for line in output.split("\n") if line])
-      return floats
-    else:
-      raise Exception("Problem with INSEL : %s" % raw)
 
-  def parse_line(self, line):
-      return self.extract([float(x) for x in line.split() if x])
+    def run(self):
+        raw = self.raw_results()
+        match = Insel.regexp.search(raw)
+        if match:
+            output = match.group(1)
+            floats = self.extract([self.parse_line(line)
+                                   for line in output.split("\n") if line])
+            return floats
+        else:
+            raise Exception("Problem with INSEL : %s" % raw)
 
-  def extract(self, array):
-    if len(array) == 1:
-      return array[0]
-    else:
-      return array
+    def parse_line(self, line):
+        return self.extract([float(x) for x in line.split() if x])
 
-  def raw_results(self):
-    #TODO: come back to cwd
-    os.chdir(Insel.dirname)
-    with self.tempfile() as file:
-      file.write(self.content())
-      file.flush()
-      os.fsync(file.fileno())
-      var = subprocess.check_output([Insel.command, file.name], shell = False)
-    return var
+    def extract(self, array):
+        if len(array) == 1:
+            return array[0]
+        else:
+            return array
 
-  def tempfile(self):
-    return tempfile.NamedTemporaryFile(mode = 'w+', suffix = Insel.extension, prefix = 'python_%s_' % self.name)
+    def raw_results(self):
+        # TODO: come back to cwd
+        os.chdir(Insel.dirname)
+        with self.tempfile() as file:
+            file.write(self.content())
+            file.flush()
+            os.fsync(file.fileno())
+            var = subprocess.check_output(
+                [Insel.command, file.name], shell=False)
+        return var
 
-  def content(self):
-    raise Exception("Implement %s.content() !" % self.__class__.__name__)
+    def tempfile(self):
+        return tempfile.NamedTemporaryFile(
+            mode='w+', suffix=Insel.extension, prefix='python_%s_' % self.name)
+
+    def content(self):
+        raise Exception("Implement %s.content() !" % self.__class__.__name__)
--- test_insel.py
+++ test_insel.py
@@ -2,9 +2,12 @@ import insel
 import unittest
 import math
 
+
 class TestBlock(unittest.TestCase):
+
     def test_pi(self):
-        self.assertAlmostEqual(insel.block('pi'), math.pi, places=6) # WTF INSEL? single float precision
+        # WTF INSEL? single float precision
+        self.assertAlmostEqual(insel.block('pi'), math.pi, places=6)
 
     def test_sum(self):
         self.assertAlmostEqual(insel.block('sum', 2), 2, places=8)
@@ -12,14 +15,21 @@ class TestBlock(unittest.TestCase):
         self.assertAlmostEqual(insel.block('sum', 2, 4, 5), 11, places=8)
 
     def test_do(self):
-        self.assertEqual(len(insel.block('do', parameters = [1, 10, 1])), 10)
+        self.assertEqual(len(insel.block('do', parameters=[1, 10, 1])), 10)
+
 
 class TestTemplate(unittest.TestCase):
+
     def test_a_times_b(self):
         self.assertAlmostEqual(insel.template('a_times_b'), 9, places=6)
         self.assertAlmostEqual(insel.template('a_times_b', a=4), 12, places=6)
-        self.assertAlmostEqual(insel.template('a_times_b', a=4, b=5), 20, places=6)
+        self.assertAlmostEqual(
+            insel.template(
+                'a_times_b',
+                a=4,
+                b=5),
+            20,
+            places=6)
 
 if __name__ == '__main__':
     unittest.main()
-
