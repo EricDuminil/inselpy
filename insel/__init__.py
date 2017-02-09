7ee07a867995687136c466969b85afa8b4f9cd42
Eric Duminil <eric.duminil@gmail.com>
2017-02-09 14:41:55 +0100

    Getting path dynamically

--- insel/__init__.py
+++ insel/__init__.py
@@ -1,11 +1,9 @@
 from one_block_model import OneBlockModel
 from template import Template
 
-
 def block(name, *args, **kwargs):
     parameters = kwargs.get('parameters', [])
     return OneBlockModel(name, inputs=args, parameters=parameters).run()
-
 
 def template(name, **parameters):
     return Template(name, **parameters).run()
--- insel/model.py
+++ insel/model.py
@@ -2,21 +2,50 @@ import os
 import subprocess
 import tempfile
 import re
+import platform
+import ConfigParser
 
 
 class Insel:
-    dirname = "/usr/local/INSEL/resources/"
-    command = "./insel"
+    def get_config():
+        system = platform.system().lower()
+
+        default_configs = {
+        'linux': {'dirname' : "/usr/local/INSEL/resources/", 'command': './insel'},
+        'windows': {'dirname' : os.path.join(os.getenv('ProgramFiles'), 'INSEL 8.2', 'resources'), 'command': 'insel.exe'},
+        'darwin': {'dirname' : "/Users/Shared", 'command': 'insel'}
+        }
+
+        if system == 'windows':
+            ini_filename = os.path.join(os.getenv('ALLUSERSPROFILE'), 'INSEL', 'inselroot.ini')
+        else:
+            ini_filename = "/Users/Shared/insel/inselroot.ini"
+
+        config = default_configs[system]
+
+        if system == 'mac':
+            subfolder = 'Contents'
+        else:
+            subfolder = 'resources'
+
+        if os.path.exists(ini_filename):
+            ini_file = ConfigParser.SafeConfigParser({'INSELROOT' : None})
+            ini_file.read(ini_filename)
+            insel_root = ini_file.get('InstallDir', 'INSELROOT')
+            if insel_root:
+                config['dirname'] = os.path.join(insel_root, subfolder)
+
+        return config
+
+    config = get_config()
+    dirname = config['dirname']
+    command = config['command']
     extension = ".insel"
     regexp = re.compile(
         'Running insel [\d\w \.]+ \.\.\.\s+([^\*]*)Normal end of run',
         re.I | re.DOTALL)
 
-# TODO: use private methods
-
-
 class Model:
-
     def run(self):
         raw = self.raw_results()
         match = Insel.regexp.search(raw)
