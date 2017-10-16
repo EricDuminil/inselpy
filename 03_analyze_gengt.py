7848fc3c56b745ab5fd8cc4c1aea69c7fe95204e
Eric Duminil <eric.duminil@gmail.com>
2017-10-16 14:45:04 +0200

    Python gengt tests

---
+++ 03_analyze_gengt.py
@@ -0,0 +1,33 @@
+import insel
+import logging
+
+from bisect import bisect_left
+
+class KeyWrapper:
+    def __init__(self, iterable, key):
+        self.it = iterable
+        self.key = key
+
+    def __getitem__(self, i):
+        return self.key(self.it[i])
+
+    def __len__(self):
+        return len(self.it)
+
+
+name = 'iterations_until_gengt_convergence'
+
+def has_enough_iterations(**params):
+    logging.disable(50)
+    model = insel.Template(name, **params)
+    model.run()
+    return not model.warnings
+
+iterations = range(100000)
+
+deviations = [5, 4, 3, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.01, 0.005]
+
+for deviation in deviations:
+    min_n = bisect_left(KeyWrapper(iterations, key= lambda n: has_enough_iterations(max_deviation = deviation,  max_iterations = n)), True)
+    dev = insel.template(name, max_deviation = deviation, max_iterations = min_n)
+    print('Max allowed deviation : %g°C, calculated in %d steps. Max deviation over 5 years: %.3f °C' % (deviation, min_n, dev))
