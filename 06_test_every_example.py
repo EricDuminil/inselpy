dc834b236c57d7c53a27b6263718a18f8b073f18
Eric Duminil <eric.duminil@gmail.com>
2021-05-04 21:22:06 +0200

    Relative paths

--- 06_test_every_example.py
+++ 06_test_every_example.py
@@ -3,6 +3,7 @@ from subprocess import TimeoutExpired, check_output
 import tempfile
 from pathlib import Path
 import unittest
+from functools import partial
 import re
 
 TIMEOUT = 10
@@ -10,28 +11,35 @@ TIMEOUT = 10
 examples = Path('/usr/local/insel/examples/')
 
 warning = re.compile(r'^[EFW]\d{5}.*?$', re.M)
+
+input_filename = re.compile(r"'([\w+_\\\/\.]+\.(dat|prn))'")
+
 normal_run = re.compile(
     r'Running insel [\d\w \.]+ \.\.\.\s+([^\*]*)Normal end of run',
     re.I | re.DOTALL)
 
-
 def replace_gnuplot(line, new_gnu):
     return line.replace("'insel.gnu'", f"'{new_gnu}'")
 
+def replace_input_filename(vseit_path, match):
+    filename = match.group(1)#.replace('\\', '/')
+    return f"'{vseit_path.parent / filename}'"
 
 def create_temp_vseit(vseit_path):
     tmp_vseit_path = TMP_DIR / vseit_path.name
-    print(vseit_path.name)
     with open(tmp_vseit_path, 'w') as tmp_vseit:
         with open(vseit_path) as vseit:
             for line in vseit:
+                line = replace_gnuplot(line, NULL_GNUPLOT)
+                line = input_filename.sub(partial(replace_input_filename, vseit_path), line)
                 tmp_vseit.write(replace_gnuplot(line, NULL_GNUPLOT))
     return tmp_vseit_path
 
 
 class TestExamples(unittest.TestCase):
-    def test_all(self):
+    def test_all_examples(self):
         for vseit_path in examples.glob('**/*.vseit'):
+            print(vseit_path)
             tmp_vseit_path = create_temp_vseit(vseit_path)
             with self.subTest(msg=vseit_path.name):
                 try:
