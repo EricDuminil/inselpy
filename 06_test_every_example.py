33f2b314a96b6b1a0e48a2e9aa783797f4cc867f
Eric Duminil <eric.duminil@gmail.com>
2021-05-04 18:14:22 +0200

    Unittests for examples

--- 06_test_every_example.py
+++ 06_test_every_example.py
@@ -1,28 +1,49 @@
 # coding=utf8
-from subprocess import run, TimeoutExpired
+from subprocess import TimeoutExpired, check_output
 import tempfile
 from pathlib import Path
+import unittest
+import re
 
 examples = Path('/usr/local/insel/examples/')
 
-def replace_gnuplot(line):
-    return line.replace("'insel.gnu'", f"'{dummy_gnuplot_path}'")
+warning = re.compile('^[EFW]\d{5}.*?$', re.M)
+normal_run = re.compile(
+    'Running insel [\d\w \.]+ \.\.\.\s+([^\*]*)Normal end of run',
+    re.I | re.DOTALL)
 
+def replace_gnuplot(line, new_gnu):
+    return line.replace("'insel.gnu'", f"'{new_gnu}'")
 
-with tempfile.TemporaryDirectory() as tmp_dir:
-    tmp_dir = Path(tmp_dir)
+class TestExamples(unittest.TestCase):
+    def test_all(self):
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            tmp_dir = Path(tmp_dir)
 
-    dummy_gnuplot_path = tmp_dir / 'dummy.gnu'
-    with open(dummy_gnuplot_path, 'w') as out:
-        out.write('set terminal unknown')
+            dummy_gnuplot_path = tmp_dir / 'dummy.gnu'
+            with open(dummy_gnuplot_path, 'w') as out:
+                out.write('set terminal unknown')
 
-    for vseit_path in examples.glob('**/*.vseit'):
-        tmp_vseit_path = tmp_dir / vseit_path.name
-        with open(tmp_vseit_path, 'w') as tmp_vseit:
-            with open(vseit_path) as vseit:
-                for line in vseit:
-                    tmp_vseit.write(replace_gnuplot(line))
-        try:
-            run(["insel", tmp_vseit_path], cwd=tmp_dir, timeout=10)
-        except TimeoutExpired:
-            print("Timeout")
+            for vseit_path in list(examples.glob('**/*.vseit'))[:15]:
+                tmp_vseit_path = tmp_dir / vseit_path.name
+                print(vseit_path.name)
+                with self.subTest(msg=vseit_path.name):
+                    with open(tmp_vseit_path, 'w') as tmp_vseit:
+                        with open(vseit_path) as vseit:
+                            for line in vseit:
+                                tmp_vseit.write(replace_gnuplot(line, dummy_gnuplot_path))
+                    try:
+                        warnings = []
+                        result = check_output(["insel", tmp_vseit_path],
+                                              cwd=tmp_dir,
+                                              timeout=10).decode()
+                        for problem in warning.findall(result):
+                            print('  ', problem)
+                            warnings.append(problem)
+                        self.assertTrue(len(warnings) == 0,
+                                        f"Errors/Warnings were found during execution of {vseit_path}")
+                    except TimeoutExpired:
+                        self.fail(f"Timeout for {vseit_path}")
+
+if __name__ == '__main__':
+    unittest.main()
