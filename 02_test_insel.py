30b2ad3e37fce39d64586245ef46441195ed78b2
Eric Duminil <eric.duminil@gmail.com>
2022-12-06 21:21:25 +0100

    Skip -1^2

--- 02_test_insel.py
+++ 02_test_insel.py
@@ -1013,9 +1013,10 @@ class TestGenericExpression(CustomAssertions):
 
     def test_power(self):
         # Used to be ^.
-        self.assertEqual(self.expr('-1^2'), 1) # That's weird, it's parsed as (-1)**2
         self.assertEqual(self.expr('2^4'), 16)
         self.assertEqual(self.expr('3^3'), 27)
+        self.skipTest("TinyExpr does exponentiation from left to right")
+        self.assertEqual(self.expr('-1^2'), -1) # but it's parsed as (-1)**2 :-/
 
     def test_one_input(self):
         self.assertAlmostEqual(self.expr('cos(x)', math.pi), -1)
