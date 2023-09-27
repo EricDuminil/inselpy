cc9fffe826050cba5e97ff433c52592b32150f31
Eric Duminil <eric.duminil@gmail.com>
2023-09-27 21:19:54 +0200

    more tests

--- src/insel/tests/test_plots.py
+++ src/insel/tests/test_plots.py
@@ -2,10 +2,14 @@ import os
 import insel
 from .custom_assertions import CustomAssertions
 from .constants import SCRIPT_DIR
+from pathlib import Path
 
 os.chdir(SCRIPT_DIR)
 
 
 class TestBasicPlots(CustomAssertions):
     def test_plot_sin(self):
+        png_output = Path('plots/sine.png')
+        png_output.unlink(missing_ok=True)
         insel.plot('plots/sine', x_max=720)
+        self.assertTrue(png_output.exists(), f"{png_output} should have been written")
