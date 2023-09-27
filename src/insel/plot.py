abfa3d63da2d90d8434547706f18d5c78d15baea
Eric Duminil <eric.duminil@gmail.com>
2023-09-27 16:05:16 +0200

    still broken

--- src/insel/plot.py
+++ src/insel/plot.py
@@ -1,21 +1,46 @@
+import os
+import re
+import tempfile
 from typing import Union
 import subprocess
 from .template import Template
 from .model import Row, Table
 
-GNUPLOT_COMMAND = 'gnuplot'
+COMMAND = 'gnuplot'
+EXT = '.gnuplot'
 
 
 class Plot(Template):
     """Insel Template + Gnuplot Template"""
+
+    def __init__(self, template_path, **parameters) -> None:
+        super().__init__(template_path, **parameters)
+        self.gnuplot_path = self.template_full_path().with_suffix(EXT)
 
     def run(self) -> Union[float, Row, Table]:
         result = super().run()
         self._plot()
         return result
 
-    def _plot(self):
-        gnuplot_path = self.template_full_path().with_suffix('.gnuplot')
+    def _plot_content(self):
+        with open(self.gnuplot_path,
+                  encoding='utf-8',
+                  errors='backslashreplace') as template:
+            content = template.read()
+            content = re.sub(Template.placeholder_pattern,
+                             self.replace_placeholders, content)
+            return content
 
-        return subprocess.check_output(
-            [GNUPLOT_COMMAND, gnuplot_path.absolute().as_posix()], shell=False)
+    def _temp_gnuplot(self):
+        return tempfile.NamedTemporaryFile(mode='w+', suffix=EXT,
+                                           prefix=f'{self.name}_', delete=False)
+
+    def _plot(self):
+        try:
+            with self._temp_gnuplot() as temp_plot:
+                temp_path = temp_plot.name
+                temp_plot.write(self._plot_content())
+            return subprocess.check_output([COMMAND, temp_path],
+                                           shell=False)
+        finally:
+            os.remove(temp_path)
