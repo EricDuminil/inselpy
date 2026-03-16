124caeff65a46cf8b82358abf28e431567a5393a
Eric Duminil <eric.duminil@gmail.com>
2026-03-16 13:38:04 +0100

    Moving encoding tests to encoding folder

--- src/insel/tests/test_templates.py
+++ src/insel/tests/test_templates.py
@@ -118,6 +118,34 @@ class TestTemplatesWithConstants(CustomAssertions):
     def test_both_placeholder_and_constant(self):
         self.assertEqual(3.7, insel.template("constants/same", x=1.2))
 
+class TestTemplatesWithDifferentEncoding(CustomAssertions):
+    def test_non_ascii_template(self):
+        utf8_template = insel.Template("encoding/a_times_b_utf8", a=2, b=2)
+        utf8_template.timeout = 5
+        self.assertEqual(utf8_template.run(), 4)
+
+        iso_template = insel.Template("encoding/a_times_b_iso8859", a=4, b=4)
+        iso_template.timeout = 5
+        self.assertEqual(iso_template.run(), 16)
+
+    def test_non_ascii_template_name(self):
+        # NOTE: that it's not Unicode on windows. Only "wide-chars" AFAIK
+        template = insel.Template("encoding/ä_tïm€ß_b", a=3, b=2)
+        self.assertEqual(template.run(), 6)
+
+    def test_space_in_template_name(self):
+        template = insel.Template("encoding/space in folder/a times b", a=617, b=2)
+        self.assertEqual(template.run(), 1234)
+
+    def test_non_ascii_in_template_folder(self):
+        template = insel.Template("encoding/Ëñçödìñg/a times b", a=2, b=2)
+        self.assertEqual(template.run(), 4)
+
+        template = insel.Template("encoding/Ëñçödìñg/x_plus_y.vseit", x=1200, y=34)
+        self.assertEqual(template.run(), 1234)
+
+        direct_run = insel.run("templates/encoding/Ëñçödìñg/x_plus_y.vseit")
+        self.assertEqual(direct_run, 123)
 
 class TestTemplates(CustomAssertions):
     def test_empty_if(self):
@@ -273,23 +301,6 @@ class TestTemplates(CustomAssertions):
         values = insel.template("mathematics/polyg")
         self.compareLists(values, [1, 1, 5, 4, -2, 0, 2, 4, 4, 4])
 
-    def test_non_ascii_template(self):
-        utf8_template = insel.Template("a_times_b_utf8", a=2, b=2)
-        utf8_template.timeout = 5
-        self.assertEqual(utf8_template.run(), 4)
-
-        iso_template = insel.Template("a_times_b_iso8859", a=4, b=4)
-        iso_template.timeout = 5
-        self.assertEqual(iso_template.run(), 16)
-
-    def test_non_ascii_template_name(self):
-        # NOTE: that it's not Unicode on windows. Only "wide-chars" AFAIK
-        utf8_template = insel.Template("io/ä_tïm€ß_b", a=2, b=2)
-        self.assertEqual(utf8_template.run(), 4)
-
-    def test_space_in_template_name(self):
-        utf8_template = insel.Template("io/space in folder/a times b", a=2, b=2)
-        self.assertEqual(utf8_template.run(), 4)
 
     def test_sunpower_isc(self):
         self.assertRaisesRegex(
