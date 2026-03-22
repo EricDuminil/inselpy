7816089bab70c4ef0387f87cbd34b03aad5b5ecd
Eric Duminil <eric.duminil@gmail.com>
2026-03-22 16:08:18 +0100

    Checking that the correct files are written

--- src/insel/tests/templates/plots/double_plot.gnuplot
+++ src/insel/tests/templates/plots/double_plot.gnuplot
@@ -1,5 +1,5 @@
 set terminal dumb 100, 40
-set output '$plot_folder$/sorted_sine_and_cos.txt'
+set output '$plot_folder$/sorted_sine_and_cosine.txt'
 
 plot "$result_folder$/insel.gpl" title "Sorted sine",\
      "$result_folder$/insel2.gpl" title "Cosine"
--- src/insel/tests/test_plots.py
+++ src/insel/tests/test_plots.py
@@ -15,4 +15,13 @@ class TestBasicPlots(CustomAssertions):
         self.assertTrue(png_output.exists(), f"{png_output} should have been written")
 
     def test_double_plot(self):
+        data_file_1 = insel.Insel.plot_path / 'insel.gpl'
+        data_file_2 = insel.Insel.plot_path / 'insel2.gpl'
+        data_file_1.unlink(missing_ok=True)
+        data_file_2.unlink(missing_ok=True)
+        txt_output = Path('plots/sorted_sine_and_cosine.txt')
+        txt_output.unlink(missing_ok=True)
         insel.plot('plots/double_plot')
+        self.assertTrue(txt_output.exists(), f"{txt_output} should have been written")
+        self.assertTrue(data_file_1.exists(), f"{data_file_1} should have been written")
+        self.assertTrue(data_file_2.exists(), f"{data_file_2} should have been written")
