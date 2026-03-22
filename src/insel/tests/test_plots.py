1824c5b50800a9206f873e729d35697f1e6972ab
Eric Duminil <eric.duminil@gmail.com>
2026-03-22 13:22:52 +0100

    Updated tests

--- src/insel/tests/test_plots.py
+++ src/insel/tests/test_plots.py
@@ -1,15 +1,20 @@
 import os
 from pathlib import Path
+
 import insel
-from .custom_assertions import CustomAssertions
+
 from .constants import SCRIPT_DIR
+from .custom_assertions import CustomAssertions
 
 os.chdir(SCRIPT_DIR)
 
 
 class TestBasicPlots(CustomAssertions):
     def test_plot_sin(self):
-        png_output = Path('plots/sine.png')
+        png_output = Path("plots/sine.png")
         png_output.unlink(missing_ok=True)
-        insel.plot('plots/sine', x_max=720)
-        self.assertTrue(png_output.exists(), f"{png_output} should have been written")
+        insel.plot("plots/sine", x_max=720)
+        self.assertTrue(
+            png_output.exists(),
+            rf"{png_output} should have been written.\ņPlease update INSEL to 8.4 if this test fails",
+        )
