a03ba0fa1fd93b401dc5c3c409f33a1c06cb3cf0
Eric Duminil <eric.duminil@gmail.com>
2026-03-22 14:21:50 +0100

    Revert "Updated tests"
    This reverts commit 1824c5b50800a9206f873e729d35697f1e6972ab.

--- src/insel/tests/test_plots.py
+++ src/insel/tests/test_plots.py
@@ -1,20 +1,15 @@
 import os
 from pathlib import Path
-
 import insel
-
-from .constants import SCRIPT_DIR
 from .custom_assertions import CustomAssertions
+from .constants import SCRIPT_DIR
 
 os.chdir(SCRIPT_DIR)
 
 
 class TestBasicPlots(CustomAssertions):
     def test_plot_sin(self):
-        png_output = Path("plots/sine.png")
+        png_output = Path('plots/sine.png')
         png_output.unlink(missing_ok=True)
-        insel.plot("plots/sine", x_max=720)
-        self.assertTrue(
-            png_output.exists(),
-            rf"{png_output} should have been written.\ņPlease update INSEL to 8.4 if this test fails",
-        )
+        insel.plot('plots/sine', x_max=720)
+        self.assertTrue(png_output.exists(), f"{png_output} should have been written")
