58c40808b49655360eb6339b97dfb873258cef0a
Eric Duminil <eric.duminil@gmail.com>
2026-05-07 15:23:35 +0200

    updated version

--- src/insel/__init__.py
+++ src/insel/__init__.py
@@ -13,7 +13,7 @@ from .photovoltaic import PhotovoltaicModuleModel as PhotovoltaicModuleModel
 from .plot import Plot
 from .template import Template
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 
 def block(
